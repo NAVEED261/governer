69ae8878a0488a95d14cfd98f60567f685df4463
NAVEED261 <146317261+NAVEED261@users.noreply.github.com>
2024-10-28 18:00:42 +0500

    Updated package.json and fixed dependencies

---
+++ src/app/governor house/page.tsx
@@ -0,0 +1,10 @@
+const GovernorPage = () => {
+    return (
+        <div>
+            <h1>Governor House Page</h1>
+            {/* Content */}
+        </div>
+    );
+};
+
+export default GovernorPage;
